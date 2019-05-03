35e4383457d42f9b133d74be03eaadf53afbe1a4
Heshy Roskes <jroskes1@jhu.edu>
2019-05-03 12:46:39 -0400

    use postprocessing

--- python/moremath.py
+++ python/moremath.py
@@ -107,9 +107,10 @@ def getquartic4dgradientstrings(coeffs):
       if coeff: derivative.append("*".join(itertools.chain((repr(coeff),), xs.elements())))
   return [" + ".join(_) + ";" for _ in derivatives]
 
-def findcriticalpointsquartic4d(coeffs, cmdline=hom4pswrapper.smallparalleltdegnopostcmdline(), verbose=False):
+def findcriticalpointsquartic4d(coeffs, cmdline=hom4pswrapper.smallparalleltdegcmdline(), verbose=False):
   p = subprocess.Popen(cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   stdin = "\n".join(["{"] + getquartic4dgradientstrings(coeffs) + ["}"])
+  if verbose: print stdin
   out, err = p.communicate(stdin)
   if "error" in err:
     raise RuntimeError("hom4ps printed an error message.\n\ninput:\n\n"+stdin+"\n\nstdout:\n\n"+out+"\n\nstderr:\n\n"+err)
@@ -136,7 +137,52 @@ def kspoissongaussian(mean, size=10000):
   ).statistic
 
 if __name__ == "__main__":
-  for size in range(0, 10000, 100):
-    if not size: continue
-    a = [kspoissongaussian(5, size=size) for i in range(100)]
-    print "{:5d} {:5.2f} {:5.2f}".format(size, np.mean(a), np.std(a))
+  array1 = np.array([float(_) for _ in """
+   337.85591457  1191.97420617  1489.68339396   878.0805623
+  -294.63832346   633.70777574  1316.31179377  1517.50734177
+  -296.22574048  1100.87258673   113.82499864  -246.34612829
+ -1264.56536224    89.16525452  2404.86523261   237.89164645
+   399.72468148   264.29193181   -60.56833793   740.82667733
+   728.48522164  -176.73949851  -303.61663869   -98.10067695
+  1431.32025212   229.49170847  -463.77617242   -53.13860056
+  -984.73589583   126.784512     713.4673213   -393.53766871
+   200.21864869 -2543.56952347     0.            61.817365
+    32.90545841  -136.84350992     3.46286231   140.8283105
+   237.5718349    -29.55247492   314.06245916   -68.93886552
+   525.60972431    82.32336621  -139.7238659    -18.96032413
+  -483.15816922    32.37615251   311.10286031  -527.40545675
+   143.83722812 -1198.39902372     0.            18.15252307
+   -62.25579809    -4.23603457    11.29049521    14.84252905
+    70.3473948    186.19764344     2.91300986  -382.01356686
+     0.           303.64025633   -79.63334502   687.43148381
+     0.            18.15252307
+  """.split()])
+  array2 = np.array([float(_) for _ in """
+   337.85591457 -1191.97420617  1489.68339396   878.0805623
+  -294.63832346   633.70777574 -1316.31179377 -1517.50734177
+   296.22574048  1100.87258673   113.82499864  -246.34612829
+ -1264.56536224    89.16525452  2404.86523261  -237.89164645
+   399.72468148   264.29193181   -60.56833793  -740.82667733
+  -728.48522164   176.73949851   303.61663869    98.10067695
+ -1431.32025212   229.49170847  -463.77617242   -53.13860056
+  -984.73589583   126.784512     713.4673213   -393.53766871
+   200.21864869 -2543.56952347     0.            61.817365
+   -32.90545841   136.84350992    -3.46286231   140.8283105
+   237.5718349    -29.55247492   314.06245916   -68.93886552
+   525.60972431   -82.32336621   139.7238659     18.96032413
+   483.15816922   -32.37615251  -311.10286031   527.40545675
+  -143.83722812  1198.39902372     0.            18.15252307
+   -62.25579809    -4.23603457    11.29049521    14.84252905
+    70.3473948    186.19764344     2.91300986  -382.01356686
+     0.           303.64025633   -79.63334502   687.43148381
+     0.            18.15252307
+  """.split()])
+
+  minimizequartic4d(array1, verbose=True, cmdline=hom4pswrapper.smallparallelcmdline())
+  minimizequartic4d(array2, verbose=True, cmdline=hom4pswrapper.smallparallelcmdline())
+
+  #minimizequartic4d(array1, verbose=True, cmdline=hom4pswrapper.smallparalleltdegcmdline())
+  #minimizequartic4d(array2, verbose=True, cmdline=hom4pswrapper.smallparalleltdegcmdline())
+
+  #minimizequartic4d(array1, verbose=True, cmdline=hom4pswrapper.smallparalleltdegnopostcmdline())
+  #minimizequartic4d(array2, verbose=True, cmdline=hom4pswrapper.smallparalleltdegnopostcmdline())
