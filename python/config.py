ba58202741e0dfc20dac0bcce01a90284d69d147
Heshy Roskes <jroskes1@jhu.edu>
2019-07-14 16:11:45 -0400

    lock all pieces before starting to make the final templates

--- python/config.py
+++ python/config.py
@@ -351,6 +351,11 @@ class TemplateBuilder(object):
         with tree:
           tree.fillall()
 
+      for template in templates:
+        for component in template.templatecomponents:
+          for piece in component.templatecomponentpieces:
+            piece.lock()
+
       for constraint in sorted(constraints, key=lambda x: x.ntemplates): #do the ones with fewer templates first because they're less likely to fail, so we won't have to remake their components if the later ones fail
         constraint.makefinaltemplates(printbins=self.__printbins, printallbins=self.__printallbins, binsortkey=self.__binsortkey)
 
